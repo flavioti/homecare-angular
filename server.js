b8e482618a67e84ff1eced5c0987b64224864009
Flavio Sousa <flaviomarcioti@gmail.com>
2017-12-04 01:43:53 -0200

    Cadastro de fisioterapeutas

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.use('/', express.static('site'));
 const PORT = process.env.PORT || 8080;
 const IP = '192.168.15.18'; //'127.0.0.1';
 
-app.listen(PORT, () => {
-    console.log(`${IP}:${PORT}`);
-});
+console.log(process.env.NODE_ENV);
+
+if ('development' === process.env.NODE_ENV) {
+    console.log('Executando no ambiente de desenvolvimento');
+    app.listen(PORT, IP, () => {
+        console.log(`${IP}:${PORT}`);
+    });
+} else {
+    app.listen(PORT, () => {
+        console.log(`${PORT}`);
+    });
+};
